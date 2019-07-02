0d311c5a006ee846f9c9c208cb9001b54e2a00db
Mike Lyons <mike@dropbox.com>
2019-07-02 18:57:24 -0400

    Complete self-made tests (whats-a-test/1.todo.js)

--- other/whats-a-test/1.todo.js
+++ other/whats-a-test/1.todo.js
@@ -19,5 +19,16 @@ error if the subtract function were to have a bug
 */
 
 // sum is intentionally broken so you can see errors in the tests
-const sum = (a, b) => a - b
+const sum = (a, b) => a + b
 const subtract = (a, b) => a - b
+
+const assertEq = (actual, expected, msg) => {
+  if  (actual === expected) {
+    console.log(`PASS: ${msg}`);
+  } else {
+    throw new Error(`${msg} (expected='${expected}', actual='${actual}')`);
+  }
+}
+
+assertEq(sum(1, 2), 3, 'Adding two integers')
+assertEq(subtract(3, 2), 1, 'Subtracting integer from another integer');
